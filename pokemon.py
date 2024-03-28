194d1a7b1fd2a282994fbab5d1c3a118fb3b74d6
alaixgg <alaix94@gmail.com>
2024-03-28 16:58:59 -0500

    asigancion de ataques a pokemons, inicio de implementacion de clases ataque y reducion de vida

--- pokemon.py
+++ pokemon.py
@@ -168,8 +168,11 @@ class Pokemon:
         self.salud = salud
         self.ataque_Behavior = ataque_Behavior
 
-    def attack(self):
-        self.ataque_Behavior.attack()
+    def attack(self, ataque, opponent):    
+        if ataque in self.ataques:
+            self.ataques[ataque].attack(opponent)
+        else:
+            print(f"{self.nombre} no conoce el ataque {ataque}.")
 
     def reduce_health(self, cantidad):
         self.health -= cantidad
@@ -180,43 +183,104 @@ class Pokemon:
         
 class Pikachu(Pokemon):
     def __init__(self):
-        super().__init__("Pikachu",100 , ataque_Behavior)
+        super().__init__("Pikachu", 100)
+        self.ataques = {
+            "Impactrueno": Impactrueno(),
+            "Rayo": Rayo(),
+            "Ataque Rápido": Ataque_Rapido(),
+            "Placaje": Placaje()
+        }
 
 class Caterpie(Pokemon):
     def __init__(self):
-        super().__init__("Caterpie",0 , ataque_Behavior)
+        super().__init__("Caterpie", 100)
+        self.ataques = {
+            "Placaje": Placaje(),
+            "Tacleada": Tacleada(),
+            "Supersónico": Supersonico(),
+            "Drenadoras": Drenadoras()
+        }
 
 class Pidgeotto(Pokemon):
     def __init__(self):
-        super().__init__("Pidgeotto",0 , ataque_Behavior)
+        super().__init__("Pidgeotto", 100)
+        self.ataques = {
+            "Picotazo": Picotazo(),
+            "Remolino": Remolino(),
+            "Tornado": Tornado(),
+            "Ataque Rápido": Ataque_Rapido()
+        }
+
 
 class Bulbasaur(Pokemon):
     def __init__(self):
-        super().__init__("Bulbasaur",0 , ataque_Behavior)
-        
+        super().__init__("Bulbasaur", 100)
+        self.ataques = {
+            "Látigo Cepa": Latigo_Cepa(),
+            "Drenadoras": Drenadoras(),
+            "Placaje": Placaje(),
+            "Somnífero": Somnifero()
+        }
+
 class Charmander(Pokemon):
     def __init__(self):
-        super().__init__("Charmander", 100, ataque_Behavior )   
+        super().__init__("Charmander", 100)
+        self.ataques = {
+            "Lanzallamas": Lanzallamas(),
+            "Gruñido": Gruñido(),
+            "Arañazo": Arañazo(),
+            "Ascuas": Ascuas()
+        }
 
 class Squirtle(Pokemon):
     def __init__(self):
-        super().__init__("Squirtle",0 , ataque_Behavior)
+        super().__init__("Squirtle", 100)
+        self.ataques = {
+            "Pistola Agua": Pistola_agua(),
+            "Burbuja": Burbuja(),
+            "Ataque Rápido": Ataque_Rapido(),
+            "Placaje": Placaje()
+        }
 
 class Krabby(Pokemon):
     def __init__(self):
-        super().__init__("Krabby",0 , ataque_Behavior)
+        super().__init__("Krabby", 100)
+        self.ataques = {
+            "Burbuja": Burbuja(),
+            "Rayo Burbuja": Rayo_Burbuja(),
+            "Placaje": Placaje(),
+            "Tajo Cruzado": Tajo_Cruzado()
+        }
 
 class Raticate(Pokemon):
     def __init__(self):
-        super().__init__("Raticate",0 , ataque_Behavior)
-        
+        super().__init__("Raticate", 100)
+        self.ataques = {
+            "Hipercolmillo": Hipercolmillo(),
+            "Ataque Rápido": Ataque_Rapido(),
+            "Placaje": Placaje(),
+            "Golpe Cabeza": Golpe_Cabeza()
+        }
+
 class Muk(Pokemon):
     def __init__(self):
-        super().__init__("Muk",0 , ataque_Behavior) 
+        super().__init__("Muk", 100)
+        self.ataques = {
+            "Lodo": Lodo(),
+            "Bomba Lodo": Bomba_lodo(),
+            "Ataque Ácido": Ataque_acido(),
+            "Infortunio": Infortunio()
+        }
 
 class Kingler(Pokemon):
     def __init__(self):
-        super().__init__("Kingler",0 , ataque_Behavior) 
+        super().__init__("Kingler", 100)
+        self.ataques = {
+            "Hidropulso": Hidropulso(),
+            "Rayo Burbuja": Rayo_Burbuja(),
+            "Rayo": Rayo(),
+            "Placaje": Placaje()
+        }
 
 """class (Pokemon):
     def __init__(self):
